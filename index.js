fc42cccf89d5ca541a6cb6cfb59910bf94a1e3b0
grrumblerr <grrumblerr@yandex.by>
2022-07-15 18:57:37 +0300

    solution

--- index.js
+++ index.js
@@ -1,16 +1,14 @@
 //---Create 2 objects: animal and cat, add move property to animal object, cat object must inherit move property from object animal
 
-let animal = {
-    move: null
+let animal = {};
+
+let cat = {
+  // __proto__: animal
 };
-let cat = Object.create(animal);
-// {
-//     __proto__: animal
-//     // move: null
-// };
+
+Object.setPrototypeOf(cat, animal);
+
 animal.move = true;
 
-// Object.setPrototypeOf(cat, animal);
-
 console.log(animal);
-console.log(cat);
+console.log(cat.move);
